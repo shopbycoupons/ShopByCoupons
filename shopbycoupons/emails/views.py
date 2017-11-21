dbacd156b44b8c5210d2172d177967fd0160b681
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-21 14:04:37 +0530

    file writing

--- shopbycoupons/emails/views.py
+++ shopbycoupons/emails/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse, HTTPRequest
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Email
 import smtplib
 import ssl
@@ -49,48 +49,10 @@ Content-Transfer-Encoding: 7bit
     printit = "Successfully sent email"
     smtp.quit()
 
-
-
     return HttpResponse(printit)
 
 def aws(request):
-    content = HTTPRequest.POST()
-
-    tag1 = 'a'
-    tag2 = 'b'
-    emailsubject = 'test'
-    emailbody = content
-
-    smtp = smtplib.SMTP()
-    smtp.connect(serviceprovider, 25)
-    smtp.starttls()
-    smtp.login(smtp1, smtp2)
-    sender = 'aggarwal.anurag@gmail.com'
-    receivers = 'aggarwal.anurag@gmail.com'
-    message = """X-SES-MESSAGE-TAGS: tagName1=""" + tag1 + """, tagName2=""" + tag2 + """
-X-SES-CONFIGURATION-SET: Track
-From: "LetsDoc" <aggarwal.anurag@gmail.com>
-To: aggarwal.anurag@gmail.com
-Subject: """ + emailsubject + """
-Content-Type: multipart/alternative;
-    boundary="----=_boundary"
-
-------=_boundary
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 7bit
-
-body
-------=_boundary
-Content-Type: text/html; charset=UTF-8
-Content-Transfer-Encoding: 7bit
-
-""" + emailbody + """
-------=_boundary--
-    """
-    smtp.sendmail(sender, receivers, message)
-    printit = "arn:aws:sns:eu-west-1:338298589935:Track"
-    smtp.quit()
-
-
-
-    return HttpResponse(status=200)
+    content = request.read()
+    file = open("C:\\users\\anurag\\shopbycoupons\\shopbycoupons\\emails\\sns.txt",'w')
+    file.write(content)
+    return HttpResponse(200)
