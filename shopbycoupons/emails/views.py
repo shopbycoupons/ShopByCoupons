43f4e558b57da200e61b4eef5fb60e466a8cbd88
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-21 10:43:15 +0530

    post testing

--- shopbycoupons/emails/views.py
+++ shopbycoupons/emails/views.py
@@ -54,4 +54,42 @@ Content-Transfer-Encoding: 7bit
 
 def aws(request):
     content = list(request.POST.items())
-    return HttpResponse(content)
+    values = dict(content)
+    tag1 = a
+    tag2 = b
+    emailsubject = test
+    emailbody = values
+
+    smtp = smtplib.SMTP()
+    smtp.connect(serviceprovider, 25)
+    smtp.starttls()
+    smtp.login(smtp1, smtp2)
+    sender = 'aggarwal.anurag@gmail.com'
+    receivers = 'aggarwal.anurag@gmail.com'
+    message = """X-SES-MESSAGE-TAGS: tagName1=""" + tag1 + """, tagName2=""" + tag2 + """
+X-SES-CONFIGURATION-SET: Track
+From: "LetsDoc" <aggarwal.anurag@gmail.com>
+To: aggarwal.anurag@gmail.com
+Subject: """ + emailsubject + """
+Content-Type: multipart/alternative;
+    boundary="----=_boundary"
+
+------=_boundary
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 7bit
+
+body
+------=_boundary
+Content-Type: text/html; charset=UTF-8
+Content-Transfer-Encoding: 7bit
+
+""" + emailbody + """
+------=_boundary--
+    """
+    smtp.sendmail(sender, receivers, message)
+    printit = "Successfully sent email"
+    smtp.quit()
+
+
+
+    return HttpResponse(printit)
