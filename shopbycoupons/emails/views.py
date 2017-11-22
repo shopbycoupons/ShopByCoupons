eee0d219ec1cb04c062c8640a633f4835893ec1c
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-22 09:37:23 +0530

    testing post request

--- shopbycoupons/emails/views.py
+++ shopbycoupons/emails/views.py
@@ -9,6 +9,7 @@ from appconfig import *
 from django.views.decorators.csrf import csrf_exempt
 import webbrowser
 import urllib.request
+import requests
 
 
 
@@ -59,40 +60,8 @@ Content-Transfer-Encoding: 7bit
 def aws(request):
     content = request.POST
     str1 = content.get('Type')
-    str2 = content.get('SubscribeURL')
-    tag1 = 'a'
-    tag2 = 'b'
-    emailsubject = 'url'
-    emailbody = str2
-
-    smtp = smtplib.SMTP()
-    smtp.connect(serviceprovider, 25)
-    smtp.starttls()
-    smtp.login(smtp1, smtp2)
-    sender = 'aggarwal.anurag@gmail.com'
-    receivers = 'aggarwal.anurag@gmail.com'
-    message = """X-SES-MESSAGE-TAGS: tagName1=""" + tag1 + """, tagName2=""" + tag2 + """
-X-SES-CONFIGURATION-SET: Track
-From: "LetsDoc" <aggarwal.anurag@gmail.com>
-To: aggarwal.anurag@gmail.com
-Subject: """ + emailsubject + """
-Content-Type: multipart/alternative;
-    boundary="----=_boundary"
-
-------=_boundary
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 7bit
-
-body
-------=_boundary
-Content-Type: text/html; charset=UTF-8
-Content-Transfer-Encoding: 7bit
-
-""" + emailbody + """
-------=_boundary--
-    """
-    smtp.sendmail(sender, receivers, message)
-    
-    smtp.quit()
-
-    return HttpResponse(str2)
+    if content.get('TopicArn')==topict:
+        r = requests.get(content.get('SubscribeURL'))
+        return HttpResponse(r)
+    else:
+        return HttpResponse('errors')
