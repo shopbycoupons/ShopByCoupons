a27c9ded0ba0db203a1f02a7121ef3b71bb1bc4c
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-23 12:02:06 +0530

    notifications

--- shopbycoupons/emails/views.py
+++ shopbycoupons/emails/views.py
@@ -59,12 +59,12 @@ Content-Transfer-Encoding: 7bit
 @csrf_exempt
 def aws(request):
     a = json.loads(request.body.decode('utf-8'))
-    b = a['notificationType']
-    c = a['mail']['destination']
+    b = a['Subject']
+    c = a['Message']
     tag1 = 'a'
     tag2 = 'b'
     emailsubject = b
-    emailbody = c[0]
+    emailbody = c
     smtp = smtplib.SMTP()
     smtp.connect(serviceprovider, 25)
     smtp.starttls()
