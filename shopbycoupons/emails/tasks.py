316c4ee8f501278de006b6bec94a3e2964261e5c
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-27 18:10:41 +0530

    test

--- shopbycoupons/emails/tasks.py
+++ shopbycoupons/emails/tasks.py
@@ -17,7 +17,7 @@ def send_email(pk, msg):
     connection = pymysql.connect(host="localhost",user=proddbuser, passwd=proddbpass, database=proddbname )
     cursor = connection.cursor()
     try:
-        cursor.execute("insert into ecamp (id, status) values (%s, %s, %s, %s)", (pk, stat))
+        cursor.execute("insert into ecamp (id, status) values (%s, %s)", (pk, stat))
         connection.commit()
     except pymysql.err.IntegrityError as e:
         if e.args[0] == 1062:
