7517866de320b5d4fb72e7b061a4acd34fba70ab
shopbycoupons <33648358+shopbycoupons@users.noreply.github.com>
2017-11-28 15:33:58 +0530

    emails

--- shopbycoupons/emails/views.py
+++ shopbycoupons/emails/views.py
@@ -20,40 +20,59 @@ def index(request):
 
 
 def email(request):
+    connection = pymysql.connect(host="localhost",user=proddbuser, passwd=proddbpass, database=proddbname )
+    cursor = connection.cursor()
+
     content = list(request.POST.items())
     values = dict(content)
     tag1 = values["tag1"]
     tag2 = values["tag2"]
     emailsubject = values["emailsubject"]
     emailbody = values["emailbody"]
+    estart = values["emailstart"]
+    noemails = values["numberofemails"]
+    iestart = int(estart)
+    inoemails = int (noemails)
+
+    cursor.execute("select email from email limit %d, %d", (iestart, inoemails))
+    emailsfromdb = (cursor.fetchall())
+    listofemails = []
+
+    for item in emailsfromdb:
+        listofemails.append(item[0])
+
+    listofemails = [x.strip() for x in listofemails]
+
 
     smtp = smtplib.SMTP()
     smtp.connect(serviceprovider, 25)
     smtp.starttls()
     smtp.login(smtp1, smtp2)
-    sender = 'aggarwal.anurag@gmail.com'
-    receivers = 'aggarwal.anurag@gmail.com'
-    message = """X-SES-MESSAGE-TAGS: tagName1=""" + tag1 + """, tagName2=""" + tag2 + """
-X-SES-CONFIGURATION-SET: Track
-From: "LetsDoc" <aggarwal.anurag@gmail.com>
-To: aggarwal.anurag@gmail.com
-Subject: """ + emailsubject + """
-Content-Type: multipart/alternative;
-    boundary="----=_boundary"
+    for item in listofemails:
+        sender = 'aggarwal.anurag@gmail.com'
+        receivers = 'aggarwal.anurag@gmail.com'
+        message = """X-SES-MESSAGE-TAGS: tagName1=""" + tag1 + """, tagName2=""" + tag2 + """
+    X-SES-CONFIGURATION-SET: Track
+    From: "LetsDoc" <aggarwal.anurag@gmail.com>
+    To: aggarwal.anurag@gmail.com
+    Subject: """ + emailsubject + """
+    Content-Type: multipart/alternative;
+        boundary="----=_boundary"
 
-------=_boundary
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 7bit
+    ------=_boundary
+    Content-Type: text/plain; charset=UTF-8
+    Content-Transfer-Encoding: 7bit
 
-body
-------=_boundary
-Content-Type: text/html; charset=UTF-8
-Content-Transfer-Encoding: 7bit
+    body
+    ------=_boundary
+    Content-Type: text/html; charset=UTF-8
+    Content-Transfer-Encoding: 7bit
 
-""" + emailbody + """
-------=_boundary--
-    """
-    smtp.sendmail(sender, receivers, message)
+    """ + emailbody + """
+    ------=_boundary--
+        """
+        smtp.sendmail(sender, receivers, message)
+
     printit = "Successfully sent email"
     smtp.quit()
 
